22e6d4a11d83783d19c89b1197a02a72de9947f8
Carlos Gimenez Berral <carlos.gimenez@external.wolterskluwer.com>
2024-11-17 14:12:02 +0100

    create vehicle domain entity

---
+++ RentACar.Domain/Vehicles/Accessory.cs
@@ -0,0 +1,11 @@
+﻿namespace RentACar.Domain.Vehicles
+{
+    public enum Accessory
+    {
+        Wifi = 1,
+        AirConditioner = 2,
+        AppleCar = 3,
+        AndroidCar = 4,
+        Maps = 5,
+    }
+}
---
+++ RentACar.Domain/Vehicles/Vehicle.cs
@@ -0,0 +1,20 @@
+﻿namespace RentACar.Domain.Vehicles
+{
+    public sealed class Vehicle
+    {
+        public Guid id { get; private set; }
+        public string? Model { get; private set; }
+        public string? Vin { get; private set; }
+        public string? Direction { get; private set; }
+        public string? Department { get; private set; }
+        public string? Province { get; private set; }
+        public string? City { get; set; }
+        public string? Country { get; private set; }
+        public decimal Amount { get; private set; }
+        public string? TypeAmount { get; private set; }
+        public decimal Maintenance { get; private set; }
+        public string? MaintenanceTypeAmount { get; private set; }
+        public DateTime? MaintenanceLastDate { get; private set; }
+        public List<Accessory> Accessories { get; private set; } = new List<Accessory>();
+    }
+}
