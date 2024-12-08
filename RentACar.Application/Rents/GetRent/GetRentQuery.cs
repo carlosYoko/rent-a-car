a63e20cacc69796a6380c4e6d15b6361358b9561
Carlos Gimenez Berral <carlos.gimenez@external.wolterskluwer.com>
2024-12-08 16:25:50 +0100

    queries

---
+++ RentACar.Application/Rents/GetRent/GetRentQuery.cs
@@ -0,0 +1,6 @@
+﻿using RentACar.Application.Abstractions.Messaging;
+
+namespace RentACar.Application.Rents.GetRent
+{
+    public sealed record GetRentQuery(Guid RentId) : IQuery<RentResponse>;
+}
---
+++ RentACar.Application/Rents/GetRent/RentResponse.cs
@@ -0,0 +1,21 @@
+﻿namespace RentACar.Application.Rents.GetRent
+{
+    public sealed class RentResponse
+    {
+        public Guid Id { get; init; }
+        public Guid UserId { get; init; }
+        public Guid VehicleId { get; init; }
+        public int Status { get; init; }
+        public decimal PriceRent { get; init; }
+        public string? CurrencyRent { get; init; }
+        public decimal PriceMaintenance { get; init; }
+        public string? CurrencyMaintenance { get; init; }
+        public decimal PriceAccesories { get; init; }
+        public string? CurrencyAccesories { get; init; }
+        public decimal PriceTotal { get; init; }
+        public string? CurrencyTotal { get; init; }
+        public DateOnly DurationStart { get; init; }
+        public DateOnly DurationEnd { get; init; }
+        public DateTime DateCreation { get; init; }
+    }
+}
