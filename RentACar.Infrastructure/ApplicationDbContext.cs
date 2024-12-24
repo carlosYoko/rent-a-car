df318c57f2244c057422e134dd8a85d21477cebb
Carlos Gimenez Berral <carlos.gimenez@external.wolterskluwer.com>
2024-12-24 11:25:36 +0100

    install dependencies

---
+++ RentACar.Infrastructure/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RentACar.Domain.Abstractions;
+
+namespace RentACar.Infrastructure
+{
+    public sealed class ApplicationDbContext : DbContext, IUnitOfWork
+    {
+
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+    }
+}
